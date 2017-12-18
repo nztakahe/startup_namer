fe0620683bfc19afcd786d59b9941c219ec43672
Michael Menzies <nztakahe@gmail.com>
2017-12-18 15:33:59 +1300

    Initial Comit

---
+++ lib/app_component.dart
@@ -0,0 +1,47 @@
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:english_words/english_words.dart';
+
+// AngularDart info: https://webdev.dartlang.org/angular
+// Components info: https://webdev.dartlang.org/components
+
+@Component(
+  selector: 'my-app',
+  styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: const [materialDirectives, CORE_DIRECTIVES],
+  providers: const [materialProviders],
+)
+
+class AppComponent implements OnInit  {
+  final savedNames = new Set<WordPair>();
+
+  List<WordPair> names = generateWordPairs().take(5).toList();
+
+  void generateNames() {
+    names = generateWordPairs().take(5).toList();
+  }
+
+  @override
+  void ngOnInit() {
+    generateNames();
+  }
+
+  void addToSaved(WordPair name) {
+    savedNames.add(name);
+    print('added $name: $savedNames');
+  }
+
+  void removeFromSaved(WordPair name) {
+    savedNames.remove(name);
+    print('removed $name: $savedNames');
+  }
+
+  void toggleSavedState(WordPair name) {
+    if (savedNames.contains(name)) {
+      removeFromSaved(name);
+      return;
+    }
+    addToSaved(name);
+  }
+}
---
+++ web/main.dart
@@ -0,0 +1,7 @@
+import 'package:angular/angular.dart';
+
+import 'package:startup_namer/app_component.dart';
+
+void main() {
+  bootstrap(AppComponent);
+}
